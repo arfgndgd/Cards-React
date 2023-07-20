721c5413df0ef2372189bb1bad0f95e89dd34616
arfgndgd <gundogduarif4@gmail.com>
2023-07-20 14:35:43 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -12,14 +12,16 @@ function App() {
       <Course
         image={Angular}
         title="Angular"
-        description="Loremfdasfasfasfasfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      fdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssfadsfafad"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+        printer took a galley of type and scrambled it to make a type specimen book"
       />
       <Course
         image={Bootstrap}
         title="Bootstrap"
-        description="Loremfdasfasfasfasfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      fdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssfadsfafad"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+        printer took a galley of type and scrambled it to make a type specimen book"
       />
       <Course
         image={Cssharp}
